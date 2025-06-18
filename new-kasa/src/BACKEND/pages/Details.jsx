5133adebd4bd718f6c7cc95ac12b21e0c95e6e4e
azize1999 <azizchatti00@gmail.com>
2025-06-18 17:44:17 +0200

    create carrousel

--- new-kasa/src/BACKEND/pages/Details.jsx
+++ new-kasa/src/BACKEND/pages/Details.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import data from '../data.json'; // Adapte le chemin si besoin
+import data from '../data.json';
 import Error from '../pages/Error.jsx';
 import './Details.scss';
 
 const Details = () => {
   const { id } = useParams();
-
-  // Cherche l'élément correspondant à l'ID
   const item = data.find(item => item.id === id);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   if (!item) {
     return <Error />;
   }
 
+  const pictures = item.pictures || [item.cover]; // fallback au cas où
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? pictures.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev === pictures.length - 1 ? 0 : prev + 1));
+  };
+
   return (
     <section className="details">
+      <div className="details__cover-wrapper">
+        <img
+          src={pictures[currentIndex]}
+          alt={item.title}
+          className="details__cover"
+        />
+        {pictures.length > 1 && (
+          <>
+           <button className="arrow left" onClick={handlePrev}>
+            <i className="fa-solid fa-chevron-left"></i>
+          </button>
+          <button className="arrow right" onClick={handleNext}>
+            <i className="fa-solid fa-chevron-right"></i>
+          </button>
+
+          </>
+        )}
+      </div>
+
       <header>
         <h1 className="details__title">{item.title}</h1>
+        <p>Ile de France - Paris 10e</p>
       </header>
       <article>
         <p className="details__description">{item.description}</p>
